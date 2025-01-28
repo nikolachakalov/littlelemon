de49388b15ddfeb026014af9635d7579e13a9f92
Nikola <chakalov.nikola@gmail.com>
2025-01-28 10:04:22 +0100

    Implemented first steps of semantic structure: Added a Nav component with header and nav HTML tags. Imported the Nav component into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
+import Nav from "./Nav.js";
 
 function App() {
   return (
-    <div className="App">
-      Littlelemon
-    </div>
+    <Nav />
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,29 @@
+//import './App.css';
+
+function Nav(props) {
+  return (
+    <>
+      <header>
+        <img src={props.logo_link} alt=""></img>
+        <nav>
+          <ul>
+            <li>
+            <a href="">Home</a>
+            </li>
+            <li>
+            <a href="">About</a>
+            </li>
+            <li>
+            <a href="">Login</a>
+            </li>
+            <li>
+            <a href="">Online reservation</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Nav;
